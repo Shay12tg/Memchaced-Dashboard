7609d3e10053a8b9fee2e4fee2b1f8a5f761d330
Shay12tg <shay12tg@gmail.com>
2016-07-29 15:24:43 +0300

    Bugfixes
    1. XSS in memcache data.
    2. Moriss.js didn't load on https.

--- index.php
+++ index.php
@@ -141,10 +141,12 @@ class Simple_memchached_dashboard{
 							$value = $this->maybe_unserialize($value);
 							if (is_object($value)|| is_array($value)){
 								$value = is_object($value)? json_decode(json_encode($value), true): $value;
-								$value = '<pre class="alert alert-warning">'.print_r($this->array_map_deep( $value,array($this,'maybe_unserialize')),true).'</pre>';
+								$value = '<pre class="alert alert-warning">'.htmlspecialchars(print_r($this->array_map_deep( $value,array($this,'maybe_unserialize')),true)).'</pre>';
+							} else {
+								$value = htmlspecialchars($value);
 							}
 							$list[$eName] = array(
-								'key'   => $eName,
+								'key'   => htmlspecialchars($eName),
 								'value' => $value,
 								'type'  => $type
 							);
@@ -467,7 +469,7 @@ class Simple_memchached_dashboard{
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-			<link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.5.1.css">
+			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 			<link rel="stylesheet" href="//cdn.datatables.net/plug-ins/a5734b29083/integration/bootstrap/3/dataTables.bootstrap.css">
 			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/alertify.js/0.3.11/alertify.core.min.css">
 			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/alertify.js/0.3.11/alertify.default.min.css">
@@ -526,7 +528,7 @@ class Simple_memchached_dashboard{
 	function footer(){
 		?>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-		<script src="http://cdn.oesmith.co.uk/morris-0.5.1.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 		<script type="text/javascript" src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
 		<script type="text/javascript" src="//cdn.datatables.net/plug-ins/a5734b29083/integration/bootstrap/3/dataTables.bootstrap.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
